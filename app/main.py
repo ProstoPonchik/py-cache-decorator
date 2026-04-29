2d453d96ed8839c922e20a087ea353381543f61f
ProstoPonchik <vlad.vislovukh@gmail.com>
2026-04-29 23:29:05 +0200

    Solved!

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
+import inspect
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    results = {}
+    sig = inspect.signature(func)
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        bound = sig.bind(*args, **kwargs)
+        bound.apply_defaults()
+        key = tuple(sorted(bound.arguments.items()))
+        if key in results:
+            print("Getting from cache")
+            return results[key]
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        results[key] = result
+        return result
+    return wrapper
