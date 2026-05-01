21b3833510268b3a79ad2eef5560e10f0d6a7116
ProstoPonchik <vlad.vislovukh@gmail.com>
2026-05-01 17:48:46 +0200

    Siplify code

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
 from typing import Callable, Any
 from functools import wraps
-import inspect
 
 
 def cache(func: Callable) -> Callable:
     results = {}
-    sig = inspect.signature(func)
 
     @wraps(func)
     def wrapper(*args, **kwargs) -> Any:
-        bound = sig.bind(*args, **kwargs)
-        bound.apply_defaults()
-        key = tuple(sorted(bound.arguments.items()))
+        key = (
+            args,
+            tuple(sorted(kwargs.items()))
+        )
         if key in results:
             print("Getting from cache")
             return results[key]
